6b8844886c6d81011abc54b5ff8dee11117a8411
Ngan(USE) <phan.thanh.ngan@use-inc.co.jp>
2024-01-13 17:13:22 +0900

    [MISC] init project

---
+++ iOSMood/iOSMood/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSMood
+//
+//  Created by Phan Thành Ngân on 2024/01/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.red)
+            Text("Hello, SwiftUI!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSMood/iOSMood/iOSMoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSMoodApp.swift
+//  iOSMood
+//
+//  Created by Phan Thành Ngân on 2024/01/13.
+//
+
+import SwiftUI
+
+@main
+struct iOSMoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
