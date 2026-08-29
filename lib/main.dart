24d95da0d656b390d6ad8ddb2f7142b68ad78486
phan.thanh.ngan <phan.thanh.ngan@use-inc.co.jp>
2026-08-29 11:53:32 +0900

    try push auto

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text('You have pushed the button this many lan:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
